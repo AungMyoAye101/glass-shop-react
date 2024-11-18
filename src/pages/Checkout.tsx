60d40caf8fae4f306c7281b57eb16e95dea8c0ec
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-18 19:25:41 +0630

    update responsive to checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,154 +1,64 @@
-import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle, FaPlusCircle, FaStar } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa6";
+import { IoMdPricetags } from "react-icons/io";
 
 const Checkout = () => {
   return (
-    <section className="min-h-screen py-6 px-6 md:px-12 flex gap-4 justify-between ">
-      <div className="flex flex-col gap-4 bg-gray-100 px-4 py-6 rounded-md shadow-md">
-        <div className="flex gap-6">
-          <img src="" alt="" className="w-40 h-32 bg-green-100 shadow" />
-          <p>produt name hdsd fsdfsdf</p>
-        </div>
-        <div className="flex gap-6">
-          <img src="" alt="" className="w-40 h-32 bg-green-100 shadow" />
-          <p>produt name hdsd fsdfsdf</p>
-        </div>
-        <div className="flex gap-6">
-          <img src="" alt="" className="w-40 h-32 bg-green-100 shadow" />
-          <p>produt name hdsd fsdfsdf</p>
-        </div>
-        <div className="flex gap-6">
-          <img src="" alt="" className="w-40 h-32 bg-green-100 shadow" />
-          <p>produt name hdsd fsdfsdf</p>
-        </div>
-      </div>
+    <section className="min-h-screen py-6 px-6 md:px-12 flex flex-col md:flex-row gap-4  items-start ">
+      <div className="flex flex-col gap-4 bg-gray-100 px-4 py-6 rounded-md shadow-md flex-1  ">
+        <h1 className="text-xl font-heading font-semibold">Shopping Cart</h1>
 
-      <div className="bg-gray-100 px-4 py-6 rounded-md shadow-md">
-        <h1>Checkout</h1>
+        {/* shopping cart section */}
 
-        <div className="flex justify-between items-center gap-4 bg-gray-50 rounded shadow-md px-4 py-2">
-          <img src="/" alt="" className="w-14 h-10 bg-red-400 " />
+        <div className="flex gap-2 justify-between border border-gray-400 rounded-md p-2 ">
+          <div className="flex flex-col md:flex-row  gap-2 ">
+            <img src="" alt="" className="w-16 md:w-24 h-14 md:h-20  shadow" />
+            <div className="flex flex-col gap-1">
+              <h1 className="text-base md:text-lg font-heading font-medium">
+                I phone 16 pro max
+              </h1>
+              <div className="text-sm md:text-base flex items-center font-body gap-2">
+                <p className="flex items-center gap-1 ">
+                  <span>4.5</span> <FaStar className="text-yellow-400" />
+                </p>
+                <p className="text-xs md:text-sm">(100 ratings)</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-2">
+            <button className="text-red-400 hover:underline">Remove</button>
 
-          <h2 className="font-body font-medium ">product name </h2>
-          <div className="flex gap-2 items-center ">
-            <button>
-              <FaPlusCircle />
-            </button>
-            <span>{1}</span>
-            <button>
-              <FaMinusCircle />
-            </button>
+            <div className="flex gap-1 items-center text-purple-500">
+              <IoMdPricetags className="text-xl " />
+              <p className="text-lg font-body ">100.88 $</p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="bg-gray-100 px-4 py-6 rounded-md shadow-md">
-        <div>
-          <h1>Deliver to Mandalay</h1>
+
+      {/* Checkout section */}
+      <div className=" min-w-80 bg-gray-100 px-4 py-6 rounded-md shadow-md flex flex-col gap-4">
+        <h1 className="text-xl font-semibold font-heading">Summary </h1>
+
+        <div className="flex justify-between items-center gap-6">
+          <h2 className="font-medium font-heading">Original Price</h2>
+          <p className="font-body">100.00 $</p>
         </div>
-      </div>
-
-      {/* <div className="max-w-3xl mx-auto rounded-md shadow-md p-6 border border-gray-400 flex flex-col gap-4">
-        <h1 className="text-xl font-semibold font-heading text-yellow-500">
-          Ckeckout your paymont
-        </h1>
-        <div className="w-full h-[2px] bg-gray-600 rounded "></div>
-        <div className="flex flex-col gap-3 ">
-          <div className="flex justify-between items-center bg-gray-50 rounded shadow-md px-4 py-2">
-            <img src="/" alt="" className="w-14 h-10 bg-red-400 " />
-
-            <h2 className="font-body font-medium ">product name </h2>
-            <div className="flex gap-2 items-center ">
-              <button>
-                <FaPlusCircle />
-              </button>
-              <span>{1}</span>
-              <button>
-                <FaMinusCircle />
-              </button>
-            </div>
-
-            <div>
-              <FaTrash />
-            </div>
-          </div>
-          <div className="flex justify-between items-center bg-gray-50 rounded shadow-md px-4 py-2">
-            <img src="/" alt="" className="w-14 h-10 bg-red-400 " />
-
-            <h2 className="font-body font-medium ">product name </h2>
-            <div className="flex gap-2 items-center ">
-              <button>
-                <FaPlusCircle />
-              </button>
-              <span>{1}</span>
-              <button>
-                <FaMinusCircle />
-              </button>
-            </div>
-
-            <div>
-              <FaTrash />
-            </div>
-          </div>
-          <div className="flex justify-between items-center bg-gray-50 rounded shadow-md px-4 py-2">
-            <img src="/" alt="" className="w-14 h-10 bg-red-400 " />
-
-            <h2 className="font-body font-medium ">product name </h2>
-            <div className="flex gap-2 items-center ">
-              <button>
-                <FaPlusCircle />
-              </button>
-              <span>{1}</span>
-              <button>
-                <FaMinusCircle />
-              </button>
-            </div>
-
-            <div>
-              <FaTrash />
-            </div>
-          </div>
-          <div className="flex justify-between items-center bg-gray-50 rounded shadow-md px-4 py-2">
-            <img src="/" alt="" className="w-14 h-10 bg-red-400 " />
-
-            <h2 className="font-body font-medium ">product name </h2>
-            <div className="flex gap-2 items-center ">
-              <button>
-                <FaPlusCircle />
-              </button>
-              <span>{1}</span>
-              <button>
-                <FaMinusCircle />
-              </button>
-            </div>
-
-            <div>
-              <FaTrash />
-            </div>
-          </div>
-          <div className="flex justify-between items-center bg-gray-50 rounded shadow-md px-4 py-2">
-            <img src="/" alt="" className="w-14 h-10 bg-red-400 " />
-
-            <h2 className="font-body font-medium ">product name </h2>
-            <div className="flex gap-2 items-center ">
-              <button>
-                <FaPlusCircle />
-              </button>
-              <span>{1}</span>
-              <button>
-                <FaMinusCircle />
-              </button>
-            </div>
-
-            <div>
-              <FaTrash />
-            </div>
-          </div>
+        <div className="flex justify-between items-center gap-6">
+          <h2 className=" font-medium font-heading">Discount Price</h2>
+          <p className=" font-body">100.00 $</p>
         </div>
-        <div className="w-full h-[2px] bg-gray-600 rounded "></div>
-        <button className="px-4 py-2 rounded-md shadow-md bg-yellow-400">
-          Buy Now
+        <div className="w-full h-[1px] bg-gray-300 "></div>
+        <div className="flex justify-between items-center">
+          <h3 className="text-lg font-semibold font-heading">
+            Total <span className="font-body font-medium">(3 items)</span>
+          </h3>
+          <span className="font-body text-lg ">300.99 $</span>
+        </div>
+        <button className="text-lg font-heading px-4 py-2 rounded-md shadow-md bg-yellow-400">
+          Complete Checkout
         </button>
-      </div> */}
+      </div>
     </section>
   );
 };
