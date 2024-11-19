873f39de7cb05379f19a7572c1e8db27e9b785d5
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-19 18:39:54 +0630

    add search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { getProduct } from "./Api";
 import { CardProp } from "./Card";
+import { Link } from "react-router-dom";
 
 const Search = () => {
   const [search, setSearch] = useState("");
 
-  const [product, setProduct] = useState<CardProp>([]);
+  const [product, setProduct] = useState<CardProp[]>([]);
 
   const productData = async () => {
     const res = await getProduct();
@@ -20,10 +21,10 @@ const Search = () => {
     setSearch(e.target.value);
   };
 
-  const filteredProduct = product.filter((product) =>
-    product.title.toLowerCase().includes(search.toLocaleLowerCase())
-  );
-  console.log(filteredProduct);
+  const filteredProduct = product.filter((product) => {
+    if (search.length < 1 || search === "") return;
+    return product.title.toLowerCase().includes(search.toLocaleLowerCase());
+  });
 
   return (
     <>
@@ -37,6 +38,25 @@ const Search = () => {
       <button className="px-2 py-1  rounded">
         <FaMagnifyingGlass className="text-yellow-500 text-lg" />
       </button>
+      <div className="absolute top-16 flex flex-col gap-1 bg-gray-100 p-2 rounded-md max-h-screen  max-w-80 overflow-hidden overflow-y-scroll">
+        {search.length > 0 &&
+          filteredProduct.map((item) => (
+            <Link
+              to={`/product/${item.id}`}
+              className="flex items-center gap-2 border rounded-md p-2 "
+              key={item.id}
+            >
+              <div className="w-10 h-8 ">
+                <img
+                  src={item.image}
+                  alt="product image"
+                  className=" w-full h-full"
+                />
+              </div>
+              <h2 className=" text-sm font-body line-clamp-1">{item.title}</h2>
+            </Link>
+          ))}
+      </div>
     </>
   );
 };
