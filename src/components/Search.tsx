ad38c13cd795ad4a31c8c7d2792829ed04fab2ea
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-19 13:17:49 +0630

    add search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,24 +1,43 @@
+import { useEffect, useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import { useSearchParams } from "react-router-dom";
+import { getProduct } from "./Api";
+import { CardProp } from "./Card";
 
 const Search = () => {
-  const [search, setSearch] = useSearchParams();
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const [search, setSearch] = useState("");
 
-    console.log(search);
+  const [product, setProduct] = useState<CardProp>([]);
+
+  const productData = async () => {
+    const res = await getProduct();
+    setProduct(res);
   };
+  useEffect(() => {
+    productData();
+  }, [search]);
+  const handleChange = (e: any) => {
+    console.log(search);
+    setSearch(e.target.value);
+  };
+
+  const filteredProduct = product.filter((product) =>
+    product.title.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+  console.log(filteredProduct);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <input
         type="text"
+        value={search}
         placeholder="Search..."
         className="rounded-md px-2 py-1   focus:outline-none flex-1"
+        onChange={handleChange}
       />
       <button className="px-2 py-1  rounded">
         <FaMagnifyingGlass className="text-yellow-500 text-lg" />
       </button>
-    </form>
+    </>
   );
 };
 
