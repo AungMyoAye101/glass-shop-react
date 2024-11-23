f36ebf10f413446e252ee155e2051d9b2ac7654c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-23 19:08:15 +0630

    add images to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,19 @@
-import mall from "../assets/ecommerce.jpg";
+import delivery from "../assets/delivery.jpg";
+import location from "../assets/location.jpg";
 
 const About = () => {
   return (
-    <section className="min-h-screen flex flex-col gap-12   py-10 px-4 md:px-12 bg-gradient-to-tr from-purple-300 to-purple-500 ">
-      <div className="flex justify-center items-center gap-10">
-        <div className="w-[40%] rounded-md">
-          <img src={mall} alt="mall photo" className="w-full h-full " />
+    <section className="min-h-screen flex flex-col gap-14   py-12 px-4 md:px-12 bg-gradient-to-tr from-purple-300 to-purple-500 ">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-6 md:gap-10 ">
+        <div className="w-full sm:w-[80%] md:w-[40%] rounded-md overflow-hidden">
+          <img
+            src={delivery}
+            alt="mall photo"
+            className="w-full h-full aspect-video"
+          />
         </div>
-        <div className="w-[40%]">
-          <p className="text-xl font-body">
+        <div className="w-full md:w-[40%]">
+          <p className="text-base md:text-xl font-body">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
             saepe velit at similique mollitia eius ut quibusdam dolorem
             voluptatem repudiandae consectetur, modi quaerat libero accusantium
@@ -16,25 +21,12 @@ const About = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-row-reverse justify-center items-center gap-10">
-        <div className="w-[40%] rounded-md">
-          <img src={mall} alt="mall photo" className="w-full h-full " />
+      <div className="flex flex-col md:flex-row-reverse justify-center items-center gap-6 md:gap-10">
+        <div className="w-full sm:w-[80%] md:w-[40%]  h-72 md:h-80 aspect-video rounded-md overflow-hidden">
+          <img src={location} alt="mall photo" className="w-full h-full " />
         </div>
-        <div className="w-[40%]">
-          <p className="text-xl font-body">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-            saepe velit at similique mollitia eius ut quibusdam dolorem
-            voluptatem repudiandae consectetur, modi quaerat libero accusantium
-            architecto dolore consequatur veritatis esse?
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-center items-center gap-10">
-        <div className="w-[40%] rounded-md">
-          <img src={mall} alt="mall photo" className="w-full h-full " />
-        </div>
-        <div className="w-[40%]">
-          <p className="text-xl font-body">
+        <div className="w-full md:w-[40%]">
+          <p className="text-base md:text-xl font-body ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
             saepe velit at similique mollitia eius ut quibusdam dolorem
             voluptatem repudiandae consectetur, modi quaerat libero accusantium
