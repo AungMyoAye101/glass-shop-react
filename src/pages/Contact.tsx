1b3be911281b43fd604836e69e90942a29c247cf
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-23 19:45:44 +0630

    update contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,14 +3,17 @@ import contact from "../assets/coustomer-service.png";
 const Contact = () => {
   return (
     <section className="h-screen flex items-center   ">
-      <div className="w-[50%] h-full space-y-6 py-10 bg-gray-100">
+      <div className="hidden md:block w-full md:w-[50%] h-full space-y-6 py-10 bg-gray-100 ">
         <h1 className="text-xl font-heading font-bold text-center">
           Please lets us know your problem
         </h1>
         <img src={contact} alt="" className="w-full h-full" />
       </div>
-      <div className="w-[50%] flex items-center justify-center">
+      <div className="w-full md:w-[50%] flex items-center justify-center relative z-10 ">
         <ContactFrom />
+      </div>
+      <div className="block md:hidden absolute inset-0 z-0 self-end">
+        <img src={contact} alt="" className="w-40 h-30" />
       </div>
     </section>
   );
