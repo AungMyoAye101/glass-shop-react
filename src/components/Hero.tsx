4b9ad2d9ceb2c8e7fcc08828be5c18387ed2d37c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-11-24 19:35:00 +0630

    update : hero slider

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,25 @@
 import { Link } from "react-router-dom";
-import jo from "../img/model-1.png";
+
+import slide from "../img/slide1.png";
+import slide2 from "../img/slide-2.png";
+import slide3 from "../img/slide3.png";
 import { useEffect, useState } from "react";
 
 const slides = [
   {
-    title: "Collection",
-    dese: "You can explore and shop many differnt collection from various barands here.",
-    image: jo,
+    title: "Welcome to Our Store",
+    dese: "Discover the best products at unbeatable prices!",
+    image: slide,
   },
   {
-    title: "Co",
-    dese: "You can explore and shop many differnt collection from various barands here.",
-    image: jo,
+    title: "Latest Collection",
+    dese: " Check out the latest trends in fashion.",
+    image: slide2,
   },
   {
-    title: "Collection3",
-    dese: "You can explore and shop many differnt collection from various barands here.",
-    image: jo,
+    title: "Special Offers",
+    dese: "Get amazing deals and discounts on selected items.",
+    image: slide3,
   },
 ];
 
@@ -30,20 +33,24 @@ const Hero = () => {
     return () => {
       clearInterval(slideInterval);
     };
-  }, [slides.length]);
+  }, []);
 
   return (
-    <main className=" max-w-8xl overflow-hidden">
-      <div className="flex w-screen ">
+    <main className=" relative max-w-8xl overflow-hidden">
+      <div className="flex w-screen bg-gradient-to-tr from-purple-100  to-purple-200">
         {slides.map((item, i) => (
           <section
             key={i}
-            className="w-screen min-h-screen relative flex-shrink-0 flex items-center justify-center gap-12 px-12 bg-gradient-to-tr from-purple-300  to-purple-500 transition-transform duration-700"
+            className="w-screen min-h-screen relative flex-shrink-0 flex items-center justify-center gap-2 md:gap-12 px-4 md:px-12  transition-transform duration-700 ease-out"
             style={{ transform: `translateX(-${currentSlide * 100}%)` }}
           >
-            <div className="flex flex-col gap-6 -full md:w-[45%]">
-              <h1 className="text-6xl font-heading font-bold ">{item.title}</h1>
-              <p className="text-2xl font-heading font-semibold">{item.dese}</p>
+            <div className="flex flex-col gap-2  md:gap-6  w-[55%] md:w-[50%]">
+              <h1 className="text-xl md:text-5xl font-heading font-bold ">
+                {item.title}
+              </h1>
+              <p className="text-lg md:text-2xl font-heading font-semibold">
+                {item.dese}
+              </p>
               <Link
                 to={"/shop"}
                 className="bg-gray-900 rounded-md shadow-md px-4 py-2 text-slate-50 self-start  hover:bg-purple-600"
@@ -51,14 +58,12 @@ const Hero = () => {
                 Shop Now
               </Link>
             </div>
-            <div className="hidden md:block">
-              <div className="bg-yellow-400 size-96 rounded-full">
-                <img
-                  src={item.image}
-                  alt="model photo"
-                  className="rounded-md absolute bottom-0 w-96"
-                />
-              </div>
+            <div className="w-[40%] md:w-[45%]">
+              <img
+                src={item.image}
+                alt="model photo"
+                className="rounded-md w-full"
+              />
             </div>
           </section>
         ))}
